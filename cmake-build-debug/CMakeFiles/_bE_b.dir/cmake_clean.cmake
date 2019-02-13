bd54d42c4bfbfa68af5afb4d5085ecfcb86efb7e
LobivanZ <47587400+LobivanZ@users.noreply.github.com>
2019-02-13 13:21:32 +0700

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/_bE_b.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/_bE_b.dir/main.c.obj"
+  "_bE_b.pdb"
+  "_bE_b.exe"
+  "_bE_b.exe.manifest"
+  "lib_bE_b.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/_bE_b.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
